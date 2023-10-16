6cbb1deefd76295c6b1d03096b60c5c5f743b1c4
Daniel Tobi <danielnyorere@gmail.com>
2023-10-16 15:34:43 +0100

    Modified the main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import 'screens/calculator_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      
+      home: CalculatorScreen(),
+    );
   }
 }
